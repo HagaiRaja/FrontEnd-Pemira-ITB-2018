0bcf3c40d74de211e07bf66471ceb14be6a6baec
Hagai Raja Sinulingga <hagairaja@gmail.com>
2018-11-01 12:06:34 +0700

    Set up screen size

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
-let win
+let win;
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600});
+    win.setFullScreen(true);
 
     // and load the index.html of the app.
     win.loadURL(url.format({

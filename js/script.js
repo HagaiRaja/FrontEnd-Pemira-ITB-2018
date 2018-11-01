29b88f8be5967987294320b35788e4df68b923a6
Hagai Raja Sinulingga <hagairaja@gmail.com>
2018-11-01 19:52:57 +0700

    Adding filter to image

--- js/script.js
+++ js/script.js
@@ -12,8 +12,12 @@ function select(index_candidate) {
     var prev = document.getElementById("selected").innerHTML;
     console.log(prev)
     var x = document.getElementsByClassName("candidate");
+    var y = document.getElementsByClassName("kawung-img");
+    var z = document.getElementsByClassName("candidate-img");
     if (prev != index_candidate) {
         x[index_candidate].classList.add("candidate-active");
+        y[index_candidate].classList.add("kawung-img-active");
+        z[index_candidate].classList.remove("filter");
         document.getElementById("selected").innerHTML = index_candidate;
     }
     else {
@@ -21,6 +25,8 @@ function select(index_candidate) {
     }
     if (prev != -1) {
         x[prev].classList.remove("candidate-active");
+        y[prev].classList.remove("kawung-img-active");
+        z[prev].classList.add("filter");
     }
 }
 

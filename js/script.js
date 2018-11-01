5eedb3eb08987124ae3372fdd306271452b1581f
Hagai Raja Sinulingga <hagairaja@gmail.com>
2018-11-01 18:05:44 +0700

    Adding on click function

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ input.addEventListener("keyup", function(event) {
     }
     alert("woi")
 });
+
+function select(index_candidate) {
+    var prev = document.getElementById("selected").innerHTML;
+    console.log(prev)
+    if (prev != index_candidate) {
+        var x = document.getElementsByClassName("candidate");
+
+        x[index_candidate].classList.add("candidate-active");
+        x[prev].classList.remove("candidate-active");
+        document.getElementById("selected").innerHTML = index_candidate;
+    }
+}
+
+// function populate() {
+//     const container = document.getElementById("candidate-container");
+//     container.innerHTML += `
+//         <div>
+//
+//         </div>
+//     `
+// }
